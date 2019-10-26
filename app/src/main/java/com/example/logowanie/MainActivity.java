8b31c0d43113e1545d6d4765d7bd39a94d67abb0
ja <cos@cos>
2019-10-26 15:51:55 +0200

    dodano costam

---
+++ app/src/main/java/com/example/logowanie/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.logowanie;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the button */
+    public void test(View v) {
+        Intent intent = new Intent(this, PoLogowanie.class);
+        startActivity(intent);
+    }
+}
